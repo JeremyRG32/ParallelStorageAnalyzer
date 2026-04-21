ca27e74293e78d6318a3b61e4748a4da607ed91a
Eidan T. <eidan.then14@gmail.com>
2026-04-21 19:12:38 -0400

    Co-authored-by-<EidanThenElizo> Moved program.cs to src

--- ParallelStorageAnalyzer/obj/Debug/net8.0/ParallelStorageAnalyzer.AssemblyInfo.cs
+++ ParallelStorageAnalyzer/obj/Debug/net8.0/ParallelStorageAnalyzer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParallelStorageAnalyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3caa2e0b37ee487f9312b149666067931eb2148c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9cfd9a15e9025057ae965d11c1e32a66cdf9bf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParallelStorageAnalyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParallelStorageAnalyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
